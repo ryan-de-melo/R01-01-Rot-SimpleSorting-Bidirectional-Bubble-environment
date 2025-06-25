6e25e5fbc43496e04fe4bc371d07027019531cdd
ryan-de-melo <jos.ryan.melo@gmail.com>
2025-06-25 09:50:23 -0300

    Bubble sort completo

--- src/main/java/sorting/simpleSorting/BubbleSort.java
+++ src/main/java/sorting/simpleSorting/BubbleSort.java
@@ -2,6 +2,8 @@ package sorting.simpleSorting;
 
 import sorting.AbstractSorting;
 
+import static util.Util.swap;
+
 /**
  * The bubble sort algorithm iterates over the array multiple times, pushing big
  * elements to the right by swapping adjacent elements, until the array is
@@ -11,7 +13,26 @@ public class BubbleSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not Implemented yet!");
+
+		if (array == null || leftIndex < 0 || rightIndex > array.length - 1) {
+			throw new RuntimeException();
+		}
+
+		boolean swapped = true;
+
+		while (swapped) {
+			swapped = false;
+
+			// { 30, 28, 7, 29, 11, 26, 4, 22, 23, 31 }
+
+			for (int i = leftIndex; i < rightIndex; i++) {
+
+				if (array[i].compareTo(array[i+1]) > 0) {
+					swapped = true;
+					swap(array, i, i+1);
+				}
+			}
+		}
+		
 	}
 }
--- src/main/java/sorting/simpleSorting/SelectionSort.java
+++ src/main/java/sorting/simpleSorting/SelectionSort.java
@@ -1,5 +1,7 @@
 package sorting.simpleSorting;
 
+import static util.Util.swap;
+
 import sorting.AbstractSorting;
 
 /**
@@ -11,7 +13,39 @@ public class SelectionSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not Implemented yet!");
+		// TODO: Falta implementar!!!
+		if (!(array == null || leftIndex < 0 || rightIndex > array.length - 1)) {
+
+			boolean swapped = true;
+
+			int indexOfSmallest = findIndexOfSmallest(array, leftIndex, rightIndex);
+
+			while (swapped) {
+
+				swapped = false;
+
+				for (int i = leftIndex; i < rightIndex; i++) {
+
+				}
+			}
+
+
+		}
+
+	}
+
+
+	public int findIndexOfSmallest(T[] array, int leftIndex, int rightIndex) {
+
+		int resultIndex = leftIndex;
+
+		for (int i = leftIndex; i < rightIndex; i++) {
+			
+			if (array[i].compareTo(array[leftIndex]) < 0) {
+				resultIndex = i;
+			}
+		}
+
+		return resultIndex;
 	}
 }
