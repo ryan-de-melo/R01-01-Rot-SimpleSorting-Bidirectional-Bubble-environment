7fe496d9c2a08df868888a124da7f28c0e198e98
ryan-de-melo <jos.ryan.melo@gmail.com>
2025-06-25 15:31:41 -0300

    Insertion sort completo

--- src/main/java/sorting/simpleSorting/InsertionSort.java
+++ src/main/java/sorting/simpleSorting/InsertionSort.java
@@ -2,6 +2,8 @@ package sorting.simpleSorting;
 
 import sorting.AbstractSorting;
 
+import static util.Util.swap;
+
 /**
  * As the insertion sort algorithm iterates over the array, it makes the
  * assumption that the visited positions are already sorted in ascending order,
@@ -12,7 +14,27 @@ public class InsertionSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not Implemented yet!");
+
+		/*
+		 * Pegar o primeiro elemento e inserir ele ordenado e assim por diante
+		 * 
+		 * { 4, 9, 3, 4, 0, 5, 1, 4 } 
+		 * 1. 4 está na posição certa
+		 * 2. 9 está na posição correta
+		 * 3. 3 não está na posição correta
+		 * 
+		 * Não faz necessariamente um swap voltando
+		 * 
+		 */
+
+		for (int i = leftIndex + 1; i < rightIndex + 1; i++) {
+			T key = array[i];
+			int j = i - 1;
+
+			while (j >= 0 && key.compareTo(array[j]) < 0) {
+				swap(array, i--, j--);
+			}
+
+		} 
 	}
 }
--- src/test/java/sorting/test/StudentSortingTest.java
+++ src/test/java/sorting/test/StudentSortingTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import sorting.AbstractSorting;
 import sorting.simpleSorting.BubbleSort;
 import sorting.simpleSorting.SelectionSort;
+import sorting.simpleSorting.InsertionSort;
 
 public class StudentSortingTest {
 
@@ -40,7 +41,7 @@ public class StudentSortingTest {
 	private void getImplementation() {
 		// TODO O aluno deve instanciar sua implementação abaixo ao invés de
 		// null
-		this.implementation = new SelectionSort<Integer>();
+		this.implementation = new InsertionSort<Integer>();
 		//Assert.fail("Implementation not provided");
 	}
 
